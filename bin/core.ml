a755fcbfa45a8300f0fb66358185dcea64ec9137
Bruno Flores <bruno@brunoflores.com.br>
2021-09-23 11:39:54 +1000

    Add isval.

--- bin/core.ml
+++ bin/core.ml
@@ -5,4 +5,10 @@ let rec isnumericval t = match t with
   | TmSucc (_, t1) -> isnumericval t1
   | _ -> false
 
+let rec isval t = match t with
+    TmTrue (_) -> true
+  | TmFalse (_) -> true
+  | t when isnumericval t -> true
+  | _ -> false
+
 let eval = ()
