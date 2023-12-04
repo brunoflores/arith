9781d9a474975082cd1a7fc228fd079b744755ae
Bruno Flores <bruno@brunoflores.com.br>
2023-12-04 11:27:24 +1000

    Upgrade.

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,3 @@
-open Core
 module E = MenhirLib.ErrorReports
 module L = MenhirLib.LexerUtil
 module A = ArithLib
@@ -13,7 +12,7 @@ let print_position outx (lexbuf : Lexing.lexbuf) =
   let fname = pos.pos_fname in
   let line = pos.pos_lnum in
   let col = pos.pos_cnum - pos.pos_bol + 1 in
-  fprintf outx "%s:%d:%d" fname line col
+  Format.fprintf outx "%s:%d:%d" fname line col
 
 let show text positions =
   E.extract text positions |> E.sanitize |> E.compress |> E.shorten 20
@@ -33,13 +32,15 @@ let string_of_term (t : Syntax.term) =
 
 let succeed (v : Syntax.term option) =
   match v with
-  | Some v -> printf "%s\n" (string_of_term (Arith.eval v))
+  | Some v -> Format.printf "%s\n" (string_of_term (Arith.eval v))
   | None -> ()
 
 let fail text buffer _ =
   let location = L.range (E.last buffer) in
-  let indication = sprintf "Syntax error %s.\n" (E.show (show text) buffer) in
-  eprintf "%s%s%!" location indication;
+  let indication =
+    Format.sprintf "Syntax error %s.\n" (E.show (show text) buffer)
+  in
+  Format.eprintf "%s%s%!" location indication;
   exit 1
 
 let parse lexbuf text =
@@ -48,14 +49,14 @@ let parse lexbuf text =
   let checkpoint = Parser.Incremental.prog lexbuf.lex_curr_p in
   try I.loop_handle succeed (fail text buffer) supplier checkpoint
   with Lexer.Error msg ->
-    fprintf stderr "%a: %s\n" print_position lexbuf msg;
+    Format.eprintf "%a: %s\n" print_position lexbuf msg;
     ()
 
 let get_contents s =
   let filename, content =
     match s with
     | None | Some "-" -> ("-", In_channel.input_all In_channel.stdin)
-    | Some filename -> (filename, In_channel.read_all filename)
+    | Some filename -> (filename, Stdio.In_channel.read_all filename)
   in
   (L.init filename (content |> Lexing.from_string), content)
 
@@ -66,7 +67,9 @@ let loop filename =
 let command =
   Command.basic ~summary:"Type-check a program"
     Command.Let_syntax.(
-      let%map_open filename = anon (maybe ("filename" %: Filename.arg_type)) in
+      let%map_open filename =
+        anon (maybe ("filename" %: Filename_unix.arg_type))
+      in
       fun () -> loop filename)
 
-let () = Command.run command
+let () = Command_unix.run command
